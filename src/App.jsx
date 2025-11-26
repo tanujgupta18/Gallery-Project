ce7b8f28544a3b5dc26f773341205b4788dc95e1
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-11-26 17:52:03 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Card from "./components/card";
 
 const App = () => {
   const [userData, setUserData] = useState([]);
@@ -20,23 +21,16 @@ const App = () => {
   );
 
   let printUserData = (
-    <h3 className="text-gray-400 text-xs">No User Available</h3>
+    <h3 className="text-gray-300 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold">
+      Loading...
+    </h3>
   );
 
   if (userData.length > 0) {
     printUserData = userData.map(function (elem, idx) {
       return (
         <div key={idx}>
-          <a href={elem.url} target="_blank">
-            <div className="h-40 w-44 overflow-hidden rounded-xl">
-              <img
-                className="h-full object-cover"
-                src={elem.download_url}
-                alt="Image"
-              />
-            </div>
-            <h2 className="font-bold text-lg">{elem.author}</h2>
-          </a>
+          <Card elem={elem} />
         </div>
       );
     });
@@ -44,22 +38,26 @@ const App = () => {
 
   return (
     <div className="bg-black overflow-auto text-white h-screen p-4">
-      <div className="flex flex-wrap gap-4 p-2">{printUserData}</div>
+      <div className="flex h-[82%] flex-wrap gap-4 p-2">{printUserData}</div>
 
       <div className="flex justify-center gap-4 items-center p-4">
         <button
+          style={{ opacity: index == 1 ? 0.6 : 1 }}
           className="bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold"
           onClick={() => {
             if (index > 0) {
               setIndex(index - 1);
+              setUserData([]);
             }
           }}
         >
           Prev
         </button>
+        <h4>Page {index}</h4>
         <button
           className="bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold"
           onClick={() => {
+            setUserData([]);
             setIndex(index + 1);
           }}
         >
